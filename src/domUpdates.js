54edf8c1accde82f860aaef2be472e4c923f45d4
David Leach <davidleach724@gmail.com>
2021-08-08 09:55:27 -0600

    Makes a dynamic function to generate 3 types of travel cards

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -25,27 +25,31 @@ let domUpdates = {
     let pendingSection = document.getElementById('pendingBody');
     let upcomingSection = document.getElementById('upcomingBody');
     let pastSection = document.getElementById('pastBody');
-    travelerData.pastTrips.forEach(trip => {
 
-    
-    pastSection.innerHTML += `
-    <div class="card-container" id="cardContainer">
-    <div class="top-card">
-      <img src=${trip.image} alt=${trip.imageAlt} class="destination-img">
-    </div>
-    <div class="bottom-card">
-      <h3>${trip.destination}</h3>
-      <br>
-      <h4>From: ${trip.startDate}</h4>
-      <h4>To: ${trip.endDate}</h4>
-      <h4>Travelers: ${trip.travelers}</h4>
-      <br>
-      <br>
-      <h5>TOTAL COST: ${trip.totalCost}</h5>
-    </div>
-  </div>`
-    })
+    this.createCardContent(travelerData.pendingTrips, pendingSection);
+    this.createCardContent(travelerData.upcomingTrips, upcomingSection);
+    this.createCardContent(travelerData.pastTrips, pastSection);
+  },
 
+  createCardContent (data, section) {
+    data.forEach(trip => {
+      section.innerHTML += `
+      <div class="card-container" id="cardContainer">
+      <div class="top-card">
+        <img src=${trip.image} alt=${trip.imageAlt} class="destination-img">
+      </div>
+      <div class="bottom-card">
+        <h3>${trip.destination}</h3>
+        <br>
+        <h4>From: ${trip.startDate}</h4>
+        <h4>To: ${trip.endDate}</h4>
+        <h4>Travelers: ${trip.travelers}</h4>
+        <br>
+        <br>
+        <h5>TOTAL COST: ${trip.totalCost}</h5>
+      </div>
+    </div>`
+      })
   }
 }
 
