92f0a5a84a35aac445ef0cc2859a8551e412558f
David Leach <davidleach724@gmail.com>
2021-08-10 10:34:08 -0600

    Reverts booking image and traveler value to default after user books a trip

--- src/scripts.js
+++ src/scripts.js
@@ -119,6 +119,7 @@ function checkUserInputs() {
       })
       .then(response => checkForError(response))
       .then(setTimeout(function() {gatherData()}, 250))
+      .then(domUpdates.revertChooseDestination())
       .catch(err => console.log(err))
     } else {
       return
